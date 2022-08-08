015591f25e80020cf5d4dad3d6015b77c994d88d
Owen Barrett <Owen.Barrett@gmail.com>
2022-08-08 03:28:54 +0000

    added equity list

--- historical_data/historical_data.py
+++ historical_data/historical_data.py
@@ -2,6 +2,7 @@ import logging
 import logging.handlers
 import os
 import time
+import requests
 from datetime import date
 from datetime import timedelta
 import pandas as pd
@@ -12,6 +13,12 @@ from alpaca.common.exceptions import APIError
 # keys required for stock historical data client
 # this are the paper trading keys
 client = StockHistoricalDataClient('PK52PMRHMCY15OZGMZLW', 'F8270IxVZS3hXdghv7ChIyQUalFRIZZxYYqMKfUh')
+headers = {
+      'APCA-API-KEY-ID':'PK52PMRHMCY15OZGMZLW',
+      'APCA-API-SECRET-KEY':'F8270IxVZS3hXdghv7ChIyQUalFRIZZxYYqMKfUh'
+}
+
+url_v2 = 'https://paper-api.alpaca.markets/v2/'
 
 handler = logging.handlers.WatchedFileHandler(
     os.environ.get("LOGFILE", "historical_data.log"))
@@ -21,15 +28,15 @@ logger = logging.getLogger(__name__)
 logger.setLevel(os.environ.get("LOGLEVEL", "INFO"))
 logger.addHandler(handler)
 
-def yesterday_string():
-  yesterday = date.today() - timedelta(days=1)
+def dayshift_string(n):
+  yesterday = date.today() - timedelta(days=n)
   return yesterday.strftime('%Y-%m-%d')
 
 def fetch_bars(stock_request):
   while True:
     try:
       bars = client.get_stock_bars(stock_request)
-      return bars
+      return bars.df
     except APIError as api_error:
       logger.error('APIError code %s', api_error.status_code)
       if api_error.status_code == 429:
@@ -37,44 +44,72 @@ def fetch_bars(stock_request):
         time.sleep(60)
       else:
         return pd.DataFrame()
+    except AttributeError as error:
+      logger.warning('Empty request')
+      return pd.DataFrame()
 
-def compile_bars(bars,number_of_years):
+def compile_bars(symbol,number_of_years):
   current_year = date.today().year
-  compiled_bars = bars
+  compiled_bars = pd.DataFrame()
   cutoff = 2015
   if current_year - number_of_years + 1 < cutoff:
     number_of_years = current_year - cutoff + 1
   if number_of_years > 0:
     request_params = StockBarsRequest(
-                          symbol_or_symbols="AAPL",
-                          timeframe=TimeFrame.Day,
+                          symbol_or_symbols=symbol,
+                          timeframe=TimeFrame.Minute,
                           limit=None,
                           start="%s-01-01" % current_year,
-                          end=yesterday_string(),
+                          end=dayshift_string(5),
                           adjustment="all"
                      )
     new_bars = fetch_bars(request_params)
-    compiled_bars = pd.concat([new_bars.df,compiled_bars])
-    logger.info('Fetched year %s', current_year)
+    compiled_bars = pd.concat([new_bars,compiled_bars])
+    logger.info('Fetched %s year %s' % (symbol, current_year))
   for i in range(1,number_of_years):
     year = current_year - i
     request_params = StockBarsRequest(
-                          symbol_or_symbols="AAPL",
-                          timeframe=TimeFrame.Day,
+                          symbol_or_symbols=symbol,
+                          timeframe=TimeFrame.Minute,
                           limit=None,
                           start="%s-01-01" % year,
                           end="%s-12-31" % year,
                           adjustment="all"
                      )
     new_bars = fetch_bars(request_params)
-    compiled_bars = pd.concat([new_bars.df,compiled_bars])
-    logger.info('Fetched year %s', year)
+    compiled_bars = pd.concat([new_bars,compiled_bars])
+    logger.info('Fetched %s year %s' % (symbol, year))
   return compiled_bars
 
+def get_shortable_equity_list():
+  asset_list = requests.get(url_v2 + 'assets', headers=headers)
+  assets_raw = asset_list.json()
+  shortable_equity_list = []
+  for element in assets_raw:
+    if element['tradable']==True and element['class']=='us_equity' and element['shortable']==True:
+      shortable_equity_list.append(element['symbol'])
+  shortable_equity_symbols = {'symbol': shortable_equity_list}
+  return pd.DataFrame(shortable_equity_symbols)
+
+
 def main():
-  bars = pd.DataFrame()
-  bars = compile_bars(bars,20)
-  bars.to_csv('AAPL.csv')
+#  shortable_list = get_shortable_equity_list();
+#  shortable_list.to_csv('shortable_equity_list.csv')
+  shortable_list_df = pd.read_csv('shortable_equity_list_in_progress.csv')
+  shortable_list = shortable_list_df['symbol'].tolist()
+  processed_list_df = pd.read_csv('shortable_equity_list_processed.csv')
+  processed_list = processed_list_df['symbol'].tolist()
+  for i in range(min(10,len(shortable_list))):
+    symbol = shortable_list.pop(0)
+    bars = compile_bars(symbol,10)
+    logger.info('symbol %s', symbol)
+    bars.to_csv('data/%s.csv' % symbol)
+    processed_list.append(symbol)
+  updated_symbol_list = pd.DataFrame({'symbol': shortable_list})
+  updated_symbol_list.to_csv('shortable_equity_list_in_progress.csv')
+  processed_symbol_list = pd.DataFrame({'symbol': processed_list})
+  processed_symbol_list.to_csv('shortable_equity_list_processed.csv')
+
 
 if __name__ == '__main__':
   main()
